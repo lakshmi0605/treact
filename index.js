5604bb12d140a96a288355f8a719a33213752c4d
Lakshmi <you@example.comlakshmipanguluri98@gmail.com>
2024-08-12 16:18:52 -0700

    Added faqs, testimonials, footer and made website responsive

---
+++ index.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    const sidebar = document.querySelector(".menu--open");
+    sidebar.style.visibility = "visible"
+  }
+  
+  function closeMenu() {
+    const sidebar = document.querySelector(".menu--open");
+    sidebar.style.visibility = "hidden"
+  }
